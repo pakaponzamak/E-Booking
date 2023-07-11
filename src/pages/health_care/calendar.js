1a700dc8faedac9f004d5f35e5de81e9cb474a54
Pakapon Phuaphoomthai <pakapon@Pakapons-MacBook-Pro.local>
2023-07-11 21:56:06 +0700

    Calendar

--- src/pages/health_care/calendar.js
+++ src/pages/health_care/calendar.js
@@ -103,7 +103,7 @@ export default function Calendar() {
       i === todayDate &&
       todayMonth === currentMonth &&
       todayYear === currentYear
-        ? "rounded-xl text-right p-2 bg-red-500 w-10 h-10 current-date text-white bg-red-500 hover:bg-red-700"
+        ? "rounded-xl text-right p-2 bg-red-500 w-10 h-10 current-date text-white bg-red-500 hover:bg-red-600"
         : isClickedDay
         ? "rounded-xl text-right p-2 bg-blue-500 w-10 h-10 hover:bg-blue-600 text-white"
         : "text-right p-2 w-10 h-10 bg-slate-200 hover:bg-blue-300 ";
