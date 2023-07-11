ea0a35f408388531c5bb6e37d56410ab3bbb13c4
Pakapon Phuaphoomthai <pakapon@Pakapons-MacBook-Pro.local>
2023-07-11 15:43:30 +0700

    Calendar

--- src/pages/health_care/calendar.js
+++ src/pages/health_care/calendar.js
@@ -121,7 +121,7 @@ export default function Calendar() {
   }
 
   return (
-    <main className={`m-3  ${bai_jamjuree.className}`}>
+    <main className={`m-3  ${bai_jamjuree.className} overflow-x: auto`}>
       <div className="mb-1">
         <p className="mr-5  flex justify-end text-sm">
           ชื่อ :&nbsp; <strong>{firstName}</strong>&nbsp; ID : &nbsp;
